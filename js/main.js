52773c2abfd5dc7ca985b73c28fbf15d2b4a5d34
drwtstc-ga <drew1886@me.com>
2022-12-05 17:39:51 -0500

    more improvements to core functions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 /*----- constants -----*/
 const hmWords = [
-    "Apple",
-    "Vehicle",
-    "Camera",
-    "iPhone",
-    "Teacher",
-    "Chef",
-    "Pilot",
-    "Farmer",
+    "table",
+    "vocal",
+    "whale",
+    "zebra",
+    "yacht",
+    "chef",
+    "pilot",
+    "night",
 ];
+ 
+// const resetBtn = document.getElementById("reset");
+// resetBtn.addEventListener("click", init);
 
 const checkedLtrsDiv = document.getElementById("checkedLtrs")
 const checkedLtrs = []
 const tableEl = document.querySelectorAll("table tr td");
 for (let i = 0; i < tableEl.length; i++){
     tableEl[i].addEventListener('click',clickNum)
+}
+
+const rdmWord = hmWords[Math.floor(Math.random() * hmWords.length)]
+
+console.log(rdmWord)
+
+let bool = [];
+let word = "someword";//word from random word function
+//setting dynamic array to null
+for (let i = 0; i < word.length; i++){
+    bool.push(null);
 }
 
 const canvas = document.querySelector('canvas');
@@ -41,5 +55,12 @@ function clickNum (evt) {
     const textNode = document.createTextNode (evt.target.value)
     letter.appendChild(textNode)
     checkedLtrsDiv.appendChild(letter)
-}
 
+    // let cLetter = "s";//getting (evt.target.value)
+    // if (word.includes(cLetter)) {
+    // bool[word.indexOf(cLetter)] = true;
+    // } else {
+   
+    // console.log("generate body part")// loss logic
+    // }
+}
