a0347ca13d749b38c5dddd601dbace2467f4661a
drwtstc-ga <drew1886@me.com>
2022-12-04 20:40:51 -0500

    12/4 commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log ('Connected?');
+/*----- constants -----*/
+const hmWords = [
+    "Apple",
+    "Vehicle",
+    "Camera",
+    "iPhone",
+    "Teacher",
+    "Chef",
+    "Pilot",
+    "Farmer"
+
+];
+
+const canvas = document.querySelector('canvas');
+const canvasCtxt = canvas.getContext('2d');
+canvasCtxt.fillStyle = "#FFFFFF";
+canvasCtxt.fillRect(100, 100, 100, 100);
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
