3fa4c123fc0d2afb4050186cf792f727c1a7281b
drwtstc-ga <drew1886@me.com>
2022-12-06 18:46:34 -0500

    making changes to JS logic

--- js/main.js
+++ js/main.js
@@ -9,6 +9,28 @@ const hmWords = [
     "pilot",
     "night",
 ];
+
+const gmCont = document.getElementById("gm-container")
+
+const head = document.createElement("div")
+const torso = document.createElement("div")
+const rightArm = document.createElement("div")
+const leftArm = document.createElement("div")
+const rightLeg = document.createElement("div")
+const leftLeg = document.createElement("div")
+
+const idArray = [
+    "head","torso","leftarm","rightarm","leftleg","rightleg"
+]
+
+const bodyPartsArray = [
+    head,
+    torso,
+    leftArm,
+    rightArm,
+    leftLeg,
+    rightLeg,
+]
  
 // const resetBtn = document.getElementById("reset");
 // resetBtn.addEventListener("click", init);
@@ -20,28 +42,21 @@ for (let i = 0; i < tableEl.length; i++){
     tableEl[i].addEventListener('click',clickNum)
 }
 
-function rdmWord () {
-    solution = hmWords[Math.floor(Math.random() * hmWords.length)]
-    console.log(rdmWord)
-}
+const rdmWord = hmWords[Math.floor(Math.random() * hmWords.length)]
+console.log(rdmWord)
 
 
 let bool = [];
-let word = "someword";//word from random word function
-//setting dynamic array to null
-for (let i = 0; i < word.length; i++){
+for (let i = 0; i < rdmWord.length; i++){
     bool.push(null);
 }
-//canvas
-const canvas = document.querySelector('canvas');
-const canvasCtxt = canvas.getContext('2d');
-canvasCtxt.fillStyle = "#FFFFFF";
-canvasCtxt.fillRect(100, 100, 100, 100);
+
+
 
 /*----- state variables -----*/
+// const resultTxt = document.getElementById("outcome");
 
-
-/*----- cached elements  -----*/
+/*---- cached elements  -----*/
 
 
 
@@ -52,17 +67,17 @@ canvasCtxt.fillRect(100, 100, 100, 100);
 function clickNum (evt) {
     checkedLtrs.push (evt.target.value)
     evt.target.style.display = "none"
-    console.log (checkedLtrs)
     const letter = document.createElement('p')
     const textNode = document.createTextNode (evt.target.value)
     letter.appendChild(textNode)
     checkedLtrsDiv.appendChild(letter)
 
-    // let cLetter = "s";//getting (evt.target.value)
-    // if (word.includes(cLetter)) {
-    // bool[word.indexOf(cLetter)] = true;
-    // } else {
-   
-    // console.log("generate body part")// loss logic
-    // }
+    if (rdmWord.includes(evt.target.value)) {
+        bool[word.indexOf(evt.target.value)] = true;
+    } else {
+        let bodyPart = bodyPartsArray.shift()
+        bodyPart.setAttribute("id", idArray.shift());
+        gmCont.appendChild(bodyPart);
+        console.log("generate body part")// loss logic
+    }
 }
