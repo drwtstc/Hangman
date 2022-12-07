cf9243d8b663d29f7f6c9763b0d6805c719e7c16
drwtstc-ga <drew1886@me.com>
2022-12-06 21:53:22 -0500

    refactored code

--- js/main.js
+++ js/main.js
@@ -1,33 +1,39 @@
 /*----- constants -----*/
 const hmWords = [
-    "table",
-    "vocal",
-    "whale",
-    "zebra",
-    "yacht",
-    "chef",
-    "pilot",
-    "night",
+    "TABLE",
+    "VOCAL",
+    "WHALE",
+    "ZEBRA ",
+    "YACHT",
+    "CHEF",
+    "PILOT",
+    "NIGHT",
 ];
 
 const gmCont = document.getElementById("gm-container")
 
 const head = document.createElement("div")
-const torso = document.createElement("div")
-const rightArm = document.createElement("div")
 const leftArm = document.createElement("div")
-const rightLeg = document.createElement("div")
+const rightArm = document.createElement("div")
+const torso = document.createElement("div")
 const leftLeg = document.createElement("div")
+const rightLeg = document.createElement("div")
+
 
 const idArray = [
-    "head","torso","leftarm","rightarm","leftleg","rightleg"
+    "head",
+    "leftarm",
+    "rightarm",
+    "torso",
+    "leftleg",
+    "rightleg",
 ]
 
 const bodyPartsArray = [
     head,
-    torso,
     leftArm,
     rightArm,
+    torso,
     leftLeg,
     rightLeg,
 ]
@@ -35,49 +41,78 @@ const bodyPartsArray = [
 // const resetBtn = document.getElementById("reset");
 // resetBtn.addEventListener("click", init);
 
-const checkedLtrsDiv = document.getElementById("checkedLtrs")
-const checkedLtrs = []
-const tableEl = document.querySelectorAll("table tr td");
-for (let i = 0; i < tableEl.length; i++){
-    tableEl[i].addEventListener('click',clickNum)
+const hiddenWord = "";
+const checkedLtrs = [];
+const errors = 10;
+
+hiddenWord = grabWord();
+
+const grabWord = function () {
+    const rdmWord = Math.floor(Math.random() * hmWords.length)
+    return hmWords[rdmWord]; 
+} 
+
+const getLtr = function (letter) {
+    letter = letter.toLowerCase();
+
+    if (checkedLtrs.indexOf(letter) > -1){
+    return;
 }
+    checkedLtrs.push(letter);
+    let isOkay = hiddenWord.indexOf(letter) > -1;
 
-const rdmWord = hmWords[Math.floor(Math.random() * hmWords.length)]
-console.log(rdmWord)
-
-
-let bool = [];
-for (let i = 0; i < rdmWord.length; i++){
-    bool.push(null);
-}
-
-
-
-/*----- state variables -----*/
-// const resultTxt = document.getElementById("outcome");
-
-/*---- cached elements  -----*/
-
-
-
-/*----- event listeners -----*/
-
-
-/*----- functions -----*/
-function clickNum (evt) {
-    checkedLtrs.push (evt.target.value)
-    evt.target.style.display = "none"
-    const letter = document.createElement('p')
-    const textNode = document.createTextNode (evt.target.value)
-    letter.appendChild(textNode)
-    checkedLtrsDiv.appendChild(letter)
-
-    if (rdmWord.includes(evt.target.value)) {
-        bool[word.indexOf(evt.target.value)] = true;
-    } else {
-        let bodyPart = bodyPartsArray.shift()
-        bodyPart.setAttribute("id", idArray.shift());
-        gmCont.appendChild(bodyPart);
-        console.log("generate body part")// loss logic
+    if (!isOkay) {
+    errors = -1;
     }
-}
+
+    return correct;
+};
+
+const showWord = function () {
+    wrdState = [];
+    wrdSplit = hiddenWord.split("");
+    wordSplit.forEach(function(letter){
+        if (checkedLtrs.indexOf(letter) > -1) {
+            wordState.push(letter);
+        } else{
+            wordState.push("_");
+        }
+    });
+    return wordState;
+};
+
+// const checkedLtrsDiv = document.getElementById("checkedLtrs")
+
+// const tableEl = document.querySelectorAll("table tr td");
+// for (let i = 0; i < tableEl.length; i++){
+//     tableEl[i].addEventListener('click',clickNum)
+// }
+
+// const rdmWord = hmWords[Math.floor(Math.random() * hmWords.length)]
+// console.log(rdmWord)
+
+
+// let bool = [];
+// for (let i = 0; i < rdmWord.length; i++){
+//     bool.push(null);
+// }
+
+
+// /*----- functions -----*/
+// function clickNum (evt) {
+//     checkedLtrs.push (evt.target.value)
+//     evt.target.style.display = "none"
+//     const letter = document.createElement('p')
+//     const textNode = document.createTextNode (evt.target.value)
+//     letter.appendChild(textNode)
+//     checkedLtrsDiv.appendChild(letter)
+
+//     if (rdmWord.includes(evt.target.value)) {
+//         bool[word.indexOf(evt.target.value)] = true;
+//     } else {
+//         let bodyPart = bodyPartsArray.shift()
+//         bodyPart.setAttribute("id", idArray.shift());
+//         gmCont.appendChild(bodyPart);
+//         console.log("generate body part")// loss logic
+//     }
+// }
