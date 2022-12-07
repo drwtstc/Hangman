8f52e5b5a01e6e2fedd7fe528d750528236ed55d
drwtstc-ga <drew1886@me.com>
2022-12-07 12:14:52 -0500

    final code refactor

--- js/main.js
+++ js/main.js
@@ -1,204 +1,121 @@
-/*----- constants -----*/
-const hmWords = [
-    "TABLE",
-    "VOCAL",
-    "WHALE",
-    "ZEBRA ",
-    "YACHT",
-    "CHEF",
-    "PILOT",
-    "NIGHT",
-];
+(function () {
+    "use strict";
+    let availLtrs, words, guessInput, guess, guessButton, ltrGuess, ltrMatched, output, man, letters, lives, currentWord, numLtrsMatched, messages;
 
-// const gmCont = document.getElementById("gm-container")
+    function setup() {
+       //game setup
+        availLtrs = "abcdefghijklmnopqrstuvwxyz";
+        lives = 6;
+        words = ["pilot", "cat", "whale", "javascript", ];
+        messages = {
+            win: 'Ayeee You Won!',
+            lose: 'Yikes! You got Hangman',
+            guessed: 'choose again',
+            validLetter: 'Only letters A-Z accepted'
+        };
+        
 
-// const head = document.createElement("div")
-// const leftArm = document.createElement("div")
-// const rightArm = document.createElement("div")
-// const torso = document.createElement("div")
-// const leftLeg = document.createElement("div")
-// const rightLeg = document.createElement("div")
+        ltrGuess = ltrMatched = '';
+        numLtrsMatched = 0;
 
+        //random word
+        currentWord = words[Math.floor(Math.random() * words.length)];
 
-// const idArray = [
-//     "head",
-//     "leftarm",
-//     "rightarm",
-//     "torso",
-//     "leftleg",
-//     "rightleg",
-// ]
+        //blank lives and output
+        output = document.getElementById("output");
+        man = document.getElementById("man");
+        guessInput = document.getElementById("letter");
 
-// const bodyPartsArray = [
-//     head,
-//     leftArm,
-//     rightArm,
-//     torso,
-//     leftLeg,
-//     rightLeg,
-// ]
- 
-// const resetBtn = document.getElementById("reset");
-// resetBtn.addEventListener("click", init);
+        man.innerHTML = 'You have ' + lives + ' lives remaining';
+        output.innerHTML = '';
 
-//////////////////////////////////////////////////////////////////////
-let hangman;
+        document.getElementById("letter").value = '';
 
-//Trying to find the right code to initialize the game 
-const init = function() {
-    initControls();
-    hangman = new hangman()
-    drawCurrentWord();
-};
-document.getElementById("restart").addEventListener("click", init)
+        guessButton = document.getElementById("guess");
+        guessInput.style.display = 'inline';
+        guessButton.style.display = 'inline';
 
-const initControls = function() {
-    document.getElementById("you-win").classList   = "hide";
-    document.getElementById("game-over").classList = "hide";
-    document.getElementById("hangman").classList   = "";
-    document.getElementById("letters").innerHTML   = "";
-}
+        /* set up display of letters in current word */
+        letters = document.getElementById("letters");
+        letters.innerHTML = '<li class="current-word">Current word:</li>';
 
-//insert letter
-const insertLtr = function (evt){
-    if (evt.keycode < 65 || evt.keycode > 90)
-    return;
-
-    let letter = string.fromCharCode(evt.keycode);
-    let correct = hangman.askLetter(letter);
-
-    if (correct !== undefined && !correct) {
-        _addLetter(letter);
-      } else {
-        drawCurrentWord();
-      }
-}
-
-//draw hangman
-const dwHangman = function () {
-    document.getElementById("the-hangman").classList += " lifes-" + (hangman.errorsLeft + 1);
-   };
-
-const insertLetter = function (event) {
-    if (!hangman || (event.keyCode < 65 || event.keyCode > 90))
-      return;
-  
-    var letter  = String.fromCharCode(event.keyCode);
-    var correct = hangman.askLetter(letter);
-  
-    if (correct !== undefined && !correct) {
-      _addLetter(letter);
-      drawHangman();
-    } else {
-      drawCurrentWord();
-    }
-    afterRound()
-};
-
-//end round
-const afterRound = function () {
-  var status = hangman.gameStatus();
-
-  if (!status)
-    return;
-
-  if(status.toLowerCase() === "you win") {
-    document.getElementById("you-win").classList = "";
-  } else {
-    drawCurrentWord(hangman.secretWord.split(""));
-    document.getElementById("game-over").classList = "";
-  }
-
-  hangman = undefined;
-};
-
-//Game logic
-const grabWord = function () {
-    const rdmWord = Math.floor(Math.random() * hmWords.length)
-    return hmWords[rdmWord]; 
-} 
-const hiddenWord = grabWord();
-const checkedLtrs = [];
-const errors = 6;
-
-const getLtr = function (letter) {
-    letter = letter.toLowerCase();
-
-    if (checkedLtrs.indexOf(letter) > -1){
-    return;
-}
-    checkedLtrs.push(letter);
-    let isOkay = hiddenWord.indexOf(letter) > -1;
-
-    if (!isOkay) {
-    errors = -1;
-    }
-
-    return correct;
-};
-
-const showWord = function () {
-    wrdState = [];
-    wrdSplit = hiddenWord.split("");
-    wordSplit.forEach(function(letter){
-        if (checkedLtrs.indexOf(letter) > -1) {
-            wordState.push(letter);
-        } else{
-            wordState.push("_");
+        let letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
         }
-    });
-    return wordState;
-};
-
-//outcomeA
-const gameFin = function() {
-    return errors === 0;
-};
-//outcomeB
-const gameConcl = function() {
-    return showWord().indexOf("_") < 0;
-};
-//Game complete outcome
-const gameOver = function() {
-    if(gameFin()) {
-        return "Ayeee You Won!";
-    } else if (gameConcl()) {
-        return "Yikes! You got Hangman"
     }
-};
 
-// const checkedLtrsDiv = document.getElementById("checkedLtrs")
+    function gameOver(win) {
+        if (win) {
+            output.innerHTML = messages.win;
+            output.classList.add('win');
+        } else {
+            output.innerHTML = messages.lose;
+            output.classList.add('error');
+        }
 
-// const tableEl = document.querySelectorAll("table tr td");
-// for (let i = 0; i < tableEl.length; i++){
-//     tableEl[i].addEventListener('click',clickNum)
-// }
-
-// const rdmWord = hmWords[Math.floor(Math.random() * hmWords.length)]
-// console.log(rdmWord)
-
-
-// let bool = [];
-// for (let i = 0; i < rdmWord.length; i++){
-//     bool.push(null);
-// }
+        guessInput.style.display = guessButton.style.display = 'none';
+        guessInput.value = '';
+    }
 
 
-// /*----- functions -----*/
-// function clickNum (evt) {
-//     checkedLtrs.push (evt.target.value)
-//     evt.target.style.display = "none"
-//     const letter = document.createElement('p')
-//     const textNode = document.createTextNode (evt.target.value)
-//     letter.appendChild(textNode)
-//     checkedLtrsDiv.appendChild(letter)
+    window.onload = setup();
 
-//     if (rdmWord.includes(evt.target.value)) {
-//         bool[word.indexOf(evt.target.value)] = true;
-//     } else {
-//         let bodyPart = bodyPartsArray.shift()
-//         bodyPart.setAttribute("id", idArray.shift());
-//         gmCont.appendChild(bodyPart);
-//         console.log("generate body part")// loss logic
-//     }
-// }
+    document.getElementById("restart").onclick = setup;
+
+    guessInput.onclick = function () {
+        this.value = '';
+    };
+
+    //click logic
+    document.getElementById('hangman').onsubmit = function (e) {
+        if (e.preventDefault) e.preventDefault();
+        output.innerHTML = '';
+        output.classList.remove('error', 'warning');
+        guess = guessInput.value;
+
+        if (guess) {
+            if (availLtrs.indexOf(guess) > -1) {
+                if ((ltrMatched && ltrMatched.indexOf(guess) > -1) || (ltrGuess && ltrGuess.indexOf(guess) > -1)) {
+                    output.innerHTML = '"' + guess.toUpperCase() + '"' + messages.guessed;
+                    output.classList.add("warning");
+                }
+                else if (currentWord.indexOf(guess) > -1) {
+                    let lettersToShow;
+                    lettersToShow = document.querySelectorAll(".letter" + guess.toUpperCase());
+
+                    for (let i = 0; i < lettersToShow.length; i++) {
+                        lettersToShow[i].classList.add("correct");
+                    }
+                    for (let j = 0; j < currentWord.length; j++) {
+                        if (currentWord.charAt(j) === guess) {
+                            numLtrsMatched += 1;
+                        }
+                    }
+                    ltrMatched += guess;
+                    if (numLtrsMatched === currentWord.length) {
+                        gameOver(true);
+                    }
+                }
+                else {
+                    ltrGuess += guess;
+                    lives--;
+                    man.innerHTML = 'You have ' + lives + ' lives remaining';
+                    if (lives === 0) gameOver();
+                }
+            }
+            //valid throw error
+            else {
+                output.classList.add('error');
+                output.innerHTML = messages.validLetter;
+            }
+        }
+        //nothing entered throw error
+        else {
+            output.classList.add('error');
+            output.innerHTML = messages.validLetter;
+        }
+        return false;
+    };
+}());
